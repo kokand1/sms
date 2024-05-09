ac5cb4a462d597f7e345fe4c3bcf9353ebecabe3
abdulloh <ziz.uz3616524@gmail.com>
2024-05-09 15:10:56 +0500

    snackbar qo'shildi

--- sms_github_proekt/lib/main.dart
+++ sms_github_proekt/lib/main.dart
@@ -3,99 +3,127 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:background_sms/background_sms.dart';
 
 void main() {
-  runApp(MyaApp());
+  runApp(const MyaApp());
 }
 
-class MyaApp extends StatefulWidget {
-  const MyaApp({super.key});
+class MyaApp extends StatelessWidget {
+  const MyaApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyaApp> createState() => _MyaAppState();
-}
-
-class _MyaAppState extends State<MyaApp> {
-  final TextEditingController phonecontroler = TextEditingController();
-  final TextEditingController smscontroler = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("sms"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: phonecontroler,
-                decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 1),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green, width: 1),
-                    ),
-                    hintText: "number"),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: smscontroler,
-                decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 1),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green, width: 1),
-                    ),
-                    hintText: "matn"),
-              )
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.send),
-            onPressed: () async {
-              if (await ispermessionGranted()) {
+      home: MyHomePage(),
+    );
+  }
+}
 
-                if((await supportCustomsim)!){
-                  for(int i = 0; i <= 1; i++){
-                    await Future.delayed(const Duration(seconds: 1)).then((value)
-                     {if(phonecontroler.text.isNotEmpty || smscontroler.text.isNotEmpty){
-                      sensms(phonecontroler.text, smscontroler.text);
-                    }});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ 
+  final TextEditingController telNumerInput = TextEditingController();
+  final TextEditingController smsMatnInput = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+       title: Text("sms"),centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: telNumerInput,
+              decoration: const InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red, width: 1),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green, width: 1),
+                ),
+                hintText: "number",
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: smsMatnInput,
+              decoration: const InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red, width: 1),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green, width: 1),
+                ),
+                hintText: "matn",
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.send),
+        onPressed: () async {
+          if (await isPermissionGranted()) {
+            bool? customSimSupport = await supportsCustomSim();
+            if (customSimSupport != null && customSimSupport) {
+              for (int i = 0; i < 1; i++) {
+                await Future.delayed(const Duration(seconds: 1)).then((value) {
+                  if (telNumerInput.text.isNotEmpty || smsMatnInput.text.isNotEmpty) {
+                    sendSMS(telNumerInput.text, smsMatnInput.text);
                   }
-                }else{
-                 
-                }
-
-              } else {
-                getpermission();
+                });
               }
-            }),
+            } else {
+              // Özel sim desteği yoksa buraya bir işlem ekleyebilirsiniz.
+            }
+          } else {
+            getPermission();
+          }
+        },
       ),
     );
   }
 
-  getpermission() async => await [Permission.sms].request();
-  Future<bool> ispermessionGranted() async =>
+  getPermission() async => await [Permission.sms].request();
+
+  Future<bool> isPermissionGranted() async =>
       await Permission.sms.status.isGranted;
-  sensms(String phoneNumber, String message, {int? simSlot}) async {
-    var resalt = await BackgroundSms.sendMessage(
+
+  sendSMS(String phoneNumber, String message, {int? simSlot}) async {
+    var result = await BackgroundSms.sendMessage(
         phoneNumber: phoneNumber, message: message, simSlot: simSlot);
-    if (resalt == SmsStatus.sent) {
-      print("sent");
+    if (result == SmsStatus.sent) {
+      showSnackBar("SMS: Shu raqam $phoneNumber ga yuborildi");
+  
     } else {
-      print("eror");
+      showSnackBar("SMS yuborilmadi");
     }
-    ;
   }
 
-  Future<bool?> get supportCustomsim async =>
+  Future<bool?> supportsCustomSim() async =>
       await BackgroundSms.isSupportCustomSim;
+
+  void showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(duration: Duration(seconds: 50),
+        content: Text(
+          message,
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromARGB(255, 24, 62, 25),
+      ),
+    );
+  }
 }
